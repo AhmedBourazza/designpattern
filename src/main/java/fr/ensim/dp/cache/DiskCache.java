a4253d6a07f5c63138f0577dc3af6d50f765feeb
Ahmed BOURAZZA <e2405908@ensimlan.univ-lemans.fr>
2025-02-14 14:28:05 +0100

    diskcach

--- src/main/java/fr/ensim/dp/cache/DiskCache.java
+++ src/main/java/fr/ensim/dp/cache/DiskCache.java
@@ -3,61 +3,79 @@ package fr.ensim.dp.cache;
 import fr.ensim.dp.cache.filter.IFilterCache;
 
 import java.io.*;
+import java.util.HashMap;
 
-public class DiskCache implements ICache{
+public class DiskCache implements ICache {
     private File cacheDir;
+    private static HashMap<String, DiskCache> instances;
+
+    private DiskCache(String mapName) {
+        cacheDir = new File(mapName);
+        if (!cacheDir.exists()) {
+            cacheDir.mkdirs();
+        }
+    }
 
     @Override
     public long size() {
-        return 0;
-    }
-
-    public DiskCache(String mapName) {
-
+        long totalSize = 0;
+        for (File file : cacheDir.listFiles()) {
+            totalSize += file.length();
+        }
+        return totalSize;
     }
 
     @Override
     public boolean add(String key, byte[] buf) {
+        if (key == null || buf == null) {
+            return false;
+        }
+
         try {
             File cacheFile = new File(cacheDir, key);
+            FileOutputStream fos = new FileOutputStream(cacheFile);
             fos.write(buf);
             fos.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            return true;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            return false;
         }
-        return false;
     }
 
     @Override
     public byte[] retreive(String key) {
-        if(key != null ) {
-            try {
-                FileInputStream fis = new FileInputStream(key);
-                byte[] buf = new byte[(int) cacheFile.length()];
-                fis.read(buf);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        if (key != null) {
+            File cacheFile = new File(cacheDir, key);
+            if (cacheFile.exists()) {
+                try (FileInputStream fis = new FileInputStream(cacheFile)) {
+                    byte[] buf = new byte[(int) cacheFile.length()];
+                    fis.read(buf);
+                    return buf;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }  else
-            return null;
+        }
+        return null;
     }
 
     @Override
     public void clear() {
-
+        for (File file : cacheDir.listFiles()) {
+            file.delete();
+        }
     }
 
     @Override
     public void setFilterCache(IFilterCache filter) {
-
     }
-    public void createFile(String mapName)
-    {
-        File f = new File(mapName);
-        f.mkdirs();
+
+    public static DiskCache getInstance(String mapName) {
+        if (instances == null) {
+            instances = new HashMap<>();
+
+        }
+        return instances.get(mapName);
     }
 }
